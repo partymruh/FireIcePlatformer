94ac99dd134be1e295bcb05fa41dc75c9e07db77
Addison <addyshuppy@gmail.com>
2020-10-01 00:48:03 -0600

    Camera Follow
    Added the CameraFollow script and two
    cameras to my scene.
    Deleted old screensplit script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject character;
+    public float top, bottom, right, left, maxCamSpeed;
+    //private bool restState;  //Perhaps we will use this later if our camera becomes more advanced.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != character.transform.position)
+        {
+            Vector3 nextPosition = Vector3.Lerp(transform.position, character.transform.position, maxCamSpeed);
+            nextPosition.z = -10;
+            transform.position = nextPosition;
+        } 
+    }
+}
