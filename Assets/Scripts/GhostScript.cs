cf0e65e5bec4bd1ebad8b86bef85f0f0f0889dcb
partymruh <42664022+partymruh@users.noreply.github.com>
2020-10-07 16:57:23 -0700

    Ghost script (first version?) added and attached

---
+++ Assets/Scripts/GhostScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostScript : MonoBehaviour
+{
+
+    GameObject ghost;
+    public float alpha;
+    public Vector3 ghostOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ghost = new GameObject();
+        SpriteRenderer ghostrend = ghost.AddComponent<SpriteRenderer>();
+        ghostrend.sprite = this.transform.GetComponent<SpriteRenderer>().sprite;
+
+        Color temp = ghostrend.color;
+        temp.a = alpha;
+        ghostrend.color = temp;
+        ghost.transform.localScale = this.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ghost.transform.position = this.transform.position + ghostOffset;
+    }
+}
