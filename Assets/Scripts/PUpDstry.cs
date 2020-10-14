e3bc76f0fee03a56a86ee3605d2d27de2f814477
u1147256 <u1147256@utah.edu>
2020-10-14 13:41:36 -0600

    Improved backgrounds

--- Assets/Scripts/PUpDstry.cs
+++ Assets/Scripts/PUpDstry.cs
@@ -21,4 +21,13 @@ public class PUpDstry : MonoBehaviour
             }
         }
     }
+
+    public void DestroyObjects()
+    {
+        Destroy(this.gameObject);
+        foreach (GameObject obj in affectedTiles)
+        {
+            Destroy(obj);
+        }
+    }
 }
