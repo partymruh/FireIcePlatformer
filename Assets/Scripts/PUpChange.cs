0258598b8633c2e0359dde676518a96aa167669d
partymruh <42664022+partymruh@users.noreply.github.com>
2020-10-28 18:36:59 -0700

    forgot to push

---
+++ Assets/Scripts/PUpChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUpChange : MonoBehaviour
+{
+
+    public Sprite becomeThisSprite;
+
+    public List<GameObject> affectedTiles;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+
+            foreach (GameObject obj in affectedTiles)
+            {
+                obj.GetComponent<SpriteRenderer>().sprite = becomeThisSprite;
+            }
+        }
+    }
+}
