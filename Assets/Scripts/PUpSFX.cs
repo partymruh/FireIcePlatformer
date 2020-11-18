106742072d63c77f402cbe83adeb36cf740697d6
Addison <addyshuppy@gmail.com>
2020-11-18 13:57:14 -0700

    Updated Sounds
    Made the ice sfx more quiet
    Made prefabs of the pickups.
    Adjusted Lvl4 Camera

--- Assets/Scripts/PUpSFX.cs
+++ Assets/Scripts/PUpSFX.cs
@@ -9,8 +9,11 @@ public class PUpSFX : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
+        {
             audioEnvironment.GetComponent<AudioSource>().PlayOneShot(collectClip);
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
 }
