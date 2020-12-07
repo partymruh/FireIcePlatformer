6b551b47a5b1c1b8129b04b18443091db1bd86d8
Addison <addyshuppy@gmail.com>
2020-12-07 01:18:23 -0700

    Volume Control Settings
    Implemented Basic Volume control Settings in Main Menu.

---
+++ Assets/Scripts/SettingsBtn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsBtn : MonoBehaviour
+{
+    public GameObject settingsMenu, mainMenu;
+
+    private void OnMouseUp()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+}
